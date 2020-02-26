0b1bccd30b16c90e90f168a02b43a23d4ed1f64a
knightlylee <642026443@qq.com>
2020-02-26 21:31:30 +0800

    将原本的key:value格式转换成数组模式

--- src/main/java/com/kilo/beantoarray/Main.java
+++ src/main/java/com/kilo/beantoarray/Main.java
@@ -1,10 +1,28 @@
 package com.kilo.beantoarray;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.Feature;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
 /**
  * Created by kilo on 2020/2/26.
  */
 public class Main {
     public static void main(String[] args) {
-        
+        test_beanToArray();
+    }
+
+    public static void test_beanToArray() {
+        Model model = new Model(1001, "gaotie");
+
+        // {"id":1001, "name":"gaotie"}
+        String text_normal = JSON.toJSONString(model);
+        System.out.println(text_normal);
+        // [1001,"gaotie"]
+        String text_beanToArray = JSON.toJSONString(model, SerializerFeature.BeanToArray);
+
+        // support beanToArray & normal mode
+        Model model1 = JSON.parseObject(text_beanToArray, Model.class, Feature.SupportArrayToBean);
+        System.out.println(model1);
     }
 }
--- src/main/java/com/kilo/beantoarray/Model.java
+++ src/main/java/com/kilo/beantoarray/Model.java
@@ -4,4 +4,30 @@ package com.kilo.beantoarray;
  * Created by kilo on 2020/2/26.
  */
 public class Model {
+    private int id;
+    private String name;
+
+    public Model() {
+    }
+
+    public Model(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
