829d1a922b7b38ff9b3210f425a35f81745a9a08
knightlylee <642026443@qq.com>
2020-02-26 21:16:09 +0800

    Initial commit

---
+++ src/main/java/com/kilo/beantoarray/Main.java
@@ -0,0 +1,10 @@
+package com.kilo.beantoarray;
+
+/**
+ * Created by kilo on 2020/2/26.
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/kilo/beantoarray/Model.java
@@ -0,0 +1,7 @@
+package com.kilo.beantoarray;
+
+/**
+ * Created by kilo on 2020/2/26.
+ */
+public class Model {
+}
